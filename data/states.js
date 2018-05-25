1fe081cb693f691bd69a49f4fa9cbc7ff56591a0
Aldo Mercado <amercado1014@gmail.com>
2018-05-25 16:28:01 -0600

    Add state data

---
+++ data/states.js
@@ -0,0 +1,40 @@
+const Nightmare = require('nightmare');
+const nightmare = Nightmare({
+  show: true,
+  webPreferences: {
+    webSecurity: false
+  }
+});
+require('nightmare-iframe-manager')(Nightmare);
+const fs = require('fs');
+
+nightmare
+  .goto('https://www.cdc.gov/widgets/diseaseandconditions/data-maps.html')
+  .enterIFrame('#DataMaps1')
+  .select('select#map-selector', 'Salmonellosis')
+  .wait('tbody td.sorting_1')
+  .evaluate(() => {
+    let states = [...document.querySelectorAll('tbody td.sorting_1')]
+      .map(element => element.innerText);
+
+    const stateData = states.map(state => {
+        return {
+          name: state
+        }
+      });
+    return stateData;
+  })
+  .end()
+  .then(result => {
+    let data = JSON.stringify(result, null, 2);
+
+    fs.writeFile('./state-data.json', data, 'utf8', err => {
+      if (err) {
+        return console.log(err);
+      }
+    })
+    console.log('Data saved to file.');
+  })
+  .catch(err => {
+    console.log(err);
+  });
