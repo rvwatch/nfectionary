23930272288f68abf5a0b081e65afddee5eabda3
Ricardo Viera <ricardoclean@gmail.com>
2018-05-27 13:59:11 -0600

    Adds multiple endpoints to give us ALL the data we need

--- server.js
+++ server.js
@@ -12,8 +12,49 @@ const port = process.env.PORT || 5000;
 app.set('port', port);
 app.locals.title = 'nfectionary';
 
+app.get('/api/v1/states', (req, res) => {
+  database('states').select()
+    .then(state => {
+        return res.status(200).json(state);
+    })
+    .catch(err => {
+      return res.status(500).json({err});
+    });
+});
 
-app.get('/api/v1/states/:id', (req, res) => {
+app.get('/api/v1/states/:state_id/diseases/:disease_id', (req, res) => {
+  database('state_diseases').where({
+    states_id: req.params.state_id,
+    diseases_id:  req.params.disease_id
+  }).select()
+    .then(state => {
+      if (state.length) {
+        return res.status(200).json(state);
+      } else {
+        return res.status(404).json('Cannot find disease or state id');
+      }
+    })
+    .catch(err => {
+      return res.status(500).json({err});
+    });
+});
+
+app.get('/api/v1/specific-disease/:id', (req, res) => {
+  database('state_diseases').where('state_diseases.diseases_id', req.params.id).select()
+    .then(disease => {
+      if (disease.length) {
+        return res.status(200).json(disease);
+      } else {
+        return res.status(404).json('Cannot find disease id');
+      }
+    })
+    .catch(err => {
+      return res.status(500).json({err});
+    });
+});
+
+
+app.get('/api/v1/state-diseases/:id', (req, res) => {
   database('state_diseases').where('state_diseases.states_id', req.params.id).select()
     .then(state => {
       if (state.length) {
