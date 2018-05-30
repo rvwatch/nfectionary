8e75836d2ee37971615cc5f293fadb93b96142da
Aldo Mercado <amercado1014@gmail.com>
2018-05-30 15:07:38 -0600

    Move getDiseaseName to own file and api folder

---
+++ client/api/getDiseaseNames.js
@@ -0,0 +1,13 @@
+export const getDiseaseNames = async () => {
+  try {
+    const response = await fetch('http://localhost:5000/api/v1/diseases');
+    const diseases = await response.json();
+    const diseaseNames = diseases.map(disease => ({
+      name: disease.name,
+      id: disease.id
+    }));
+    return diseaseNames;
+  } catch (err) {
+    throw new Error(err.message);
+  }
+};
