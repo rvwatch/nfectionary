1d2da12b9a99976474dbdbf6f210b0c6a4056cd4
Aldo Mercado <amercado1014@gmail.com>
2018-05-30 15:07:12 -0600

    Move getDiseaseCount to own file in api folder

---
+++ client/api/getDiseaseCount.js
@@ -0,0 +1,13 @@
+export const getDiseaseCount = async (id) => {
+  try {
+    const response = await fetch(`http://localhost:5000/api/v1/state-diseases/${id}`);
+    const diseases = await response.json();
+    const diseaseCount = diseases.map(disease => ({ 
+      count: disease.case_count, 
+      id: disease.diseases_id 
+    }));
+    return diseaseCount
+  } catch(err) {
+    throw new Error(err.message);
+  }
+};
