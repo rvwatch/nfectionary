4ba9eadd4afe9999c7e211c1961cc27747ee82f6
Ngoc Phan <nphan4@yahoo.com>
2018-05-30 18:00:14 -0600

    Add an id into object

--- client/api/cleaners/buttonCleaner.js
+++ client/api/cleaners/buttonCleaner.js
@@ -5,6 +5,7 @@ export const buttonCleaner = (names, count) => {
       if (num.id === name.id) {
         newObject['name'] = name.name;
         newObject['count'] = num.count;
+        newObject['disease_id'] = num.id;
       }
       if (newObject.name) {
         return accu.push(newObject);
