c7736c478a5438b13a91efbc611515b981b22234
Ngoc Phan <nphan4@yahoo.com>
2018-05-30 18:35:36 -0600

    Fetch and display specific disease info on display page

--- client/api/getDisease.js
+++ client/api/getDisease.js
@@ -1,1 +1,11 @@
-
+export const getDisease = async id => {
+  try {
+    const response = await fetch(
+      `http://localhost:5000/api/v1/diseases/${id}`
+    );
+    const disease = await response.json();
+    return disease;
+  } catch (err) {
+    throw new Error(err.message);
+  }
+};
