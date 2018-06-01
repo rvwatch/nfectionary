e215eb5e7f6875a44b4ac1dd9003f228abdc805b
Aldo Mercado <amercado1014@gmail.com>
2018-06-01 17:21:48 -0600

    Fix get disease tests

--- client/api/tests/getDisease.test.js
+++ client/api/tests/getDisease.test.js
@@ -2,17 +2,35 @@ import { getDisease } from '../getDisease';
 import * as mock from '../../mockData/mockData';
 
 describe('fetch disease info', () => {
+
   beforeEach(() => {
     global.fetch = jest.fn().mockImplementation(() => {
-      Promise.resolve({
+      return Promise.resolve({
         ok: true,
-        json: () => Promise.resolve(mock.diseasesObject)
+        json: () => Promise.resolve(mock.diseaseArrayInfo)
       });
     });
   });
 
-  it('should call fetch', () => {
+  it('should call fetch', async () => {
     getDisease();
-    expect(global.fetch).toHaveBeenCalled();
+    await expect(global.fetch).toHaveBeenCalled();
+  });
+
+  it('should return fetched data', async () => {
+    const expected = mock.diseaseArrayInfo;
+
+    await expect(getDisease()).resolves.toEqual(expected);
+  });
+
+  it('should throw an error if fetch failed', async () => {
+    const expected = Error(`Error fetching disease information: Error`);
+
+    global.fetch = jest.fn().mockImplementation(() => {
+      return Promise.reject ({
+        message: 'Error'
+      });
+    });
+    await expect(getDisease()).rejects.toEqual(expected);
   });
 });
