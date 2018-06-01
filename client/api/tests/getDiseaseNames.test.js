5a0844eb77643b28ad54f73e5b922b5aed033623
Aldo Mercado <amercado1014@gmail.com>
2018-06-01 17:22:32 -0600

    Fix get disease name tests

---
+++ client/api/tests/getDiseaseNames.test.js
@@ -0,0 +1,37 @@
+import { getDiseaseNames } from '../getDiseaseNames';
+import * as mock from '../../mockData/mockData';
+
+describe('fetch disease names', () => {
+  
+  beforeEach(() => {
+    global.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve(mock.diseaseNames)
+      });
+    });
+  });
+
+  it('should call fetch', async () => {
+    getDiseaseNames();
+    await expect(global.fetch).toHaveBeenCalled();
+  });
+
+  it('should return fetched data', async () => {
+    const expected = mock.diseaseNames;
+
+    await expect(getDiseaseNames()).resolves.toEqual(expected);
+  });
+
+  it('should throw an error if fetch failed', async () => {
+    const expected = Error('Error fetching disease names: Error');
+
+    global.fetch = jest.fn().mockImplementation(() => {
+      return Promise.reject ({
+        message: 'Error'
+      });
+    });
+    await expect(getDiseaseNames()).rejects.toEqual(expected);
+  });
+})
+
