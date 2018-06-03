00d3c3d77b627fb0c9973b7b14f2bd19355ca6c3
Aldo Mercado <amercado1014@gmail.com>
2018-06-03 14:19:32 -0600

    Add home screen tests

--- client/containers/HomeScreen/HomeScreen.test.js
+++ client/containers/HomeScreen/HomeScreen.test.js
@@ -2,24 +2,47 @@ import React from 'react';
 import HomeScreen from './HomeScreen';
 import Enzyme from 'enzyme';
 import { shallow } from 'enzyme';
-import renderer from 'react-test-renderer';
+import { getStates } from '../../api/getStates';
+import * as mock from '../../mockData/mockData';
 import Adapter from 'enzyme-adapter-react-16';
 Enzyme.configure({ adapter: new Adapter() });
 
+jest.mock('../../api/getStates');
 
 describe('HomeScreen', () => {
-  test('renders correctly', () => {
-    const wrapper = renderer.create(<HomeScreen />).toJSON();
+
+  beforeEach(() => {
+    wrapper = shallow(<HomeScreen />);
+    instance = wrapper.instance();
+  });
+  
+  it('should match the snapshot', () => {
     expect(wrapper).toMatchSnapshot();
   });
 
-  it('should exist', () => {
-    const wrapper = renderer.create(<HomeScreen />).toJSON();
-    expect(wrapper).toBeDefined();
+  it('should have a default state', () => {
+    expect(wrapper.state()).toEqual({
+      response: '',
+      latitude: null,
+      longitude: null,
+      states: [],
+      error: null
+    });
   });
 
-  it('should have a state with the property of weatherResults set to null', () => {
-    const wrapper = Enzyme.shallow(<HomeScreen />);
-    expect(wrapper.state().error).toEqual(null);
+  it('should call getStates on componentDidMount', () => {
+    expect(getStates).toHaveBeenCalled()
+  });
+
+  it('should call setStatesData on componentDidMount', async () => {
+    const spy = jest.spyOn(instance, 'setStatesData');
+    await instance.componentDidMount();
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should set state with states on setStatesData', () => {
+    const states = mock.stateArray;
+    instance.setStatesData(states);
+    expect(wrapper.state('states')).toEqual(states);
   });
 });
